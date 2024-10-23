589fc64804c857ae8a002432bab5bba8fce954cf
blax <ahriman2@mail.ru>
2024-10-23 11:56:54 +0300

    Finished Upper exercise

--- upper.py
+++ upper.py
@@ -5,4 +5,26 @@ from pprint import pprint
 
 upperDict = dict(zip(string.ascii_lowercase, string.ascii_uppercase))
 
-pprint(upperDict['m'])
+def getUppercase(line):
+  upperLine = ''
+  
+  for letter in line:
+    if letter in upperDict:
+      upperLine += upperDict[letter]
+    else:
+      upperLine += letter
+  return upperLine
+  
+assert getUppercase('Hello') == 'HELLO'
+
+assert getUppercase('hello') == 'HELLO'
+
+assert getUppercase('HELLO') == 'HELLO'
+
+assert getUppercase('Hello, world!') == 'HELLO, WORLD!'
+
+assert getUppercase('goodbye 123!') == 'GOODBYE 123!'
+
+assert getUppercase('12345') == '12345'
+
+assert getUppercase('') == ''
